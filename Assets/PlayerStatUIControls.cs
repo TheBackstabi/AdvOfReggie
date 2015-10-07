23ba3f0b992470fbde33df1d88606df4398b8aae
AEMcIntosh <aemcintosh@fullsail.edu>
2015-10-07 19:19:02 -0400

    Innit Commit

---
+++ Assets/PlayerStatUIControls.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerStatUIControls : MonoBehaviour {
+
+    [SerializeField]
+    Slider stamina;
+
+    [SerializeField]
+    Image health;
+
+    [SerializeField]
+    PlayerStats player;
+
+    private int healthwidth = 230;
+    private int healthcount;
+    private int staminacount;
+
+	// Use this for initialization
+	void Start () 
+    {
+        healthcount = player.hitcount;
+        stamina.value = stamina.maxValue;
+        //Need to add hit count from player or some interaction from that.
+        health.rectTransform.sizeDelta = new Vector2(healthwidth * healthcount,health.rectTransform.sizeDelta.y);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        stamina.value = player.availstamina;
+        if(healthcount != player.hitcount)
+        health.rectTransform.sizeDelta = new Vector2(healthwidth, health.rectTransform.sizeDelta.y);
+	    
+	}
+}
---
+++ Assets/PlayerStats.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour {
+
+	// Use this for initialization
+
+    public int hitcount = 3;
+    public int availstamina = 100;
+    public GameObject player;
+    private BoxCollider2D collision;
+	void Start () 
+    {
+        collision = player.GetComponent<BoxCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    
+	}
+}
