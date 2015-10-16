854020bc8b7e982b3ef2466224b328310df083ac
Saias Gonzalez <sgonzalezvalle@fullsail.edu>
2015-10-16 03:39:59 -0400

    Revisions made

--- Assets/Scripts/BounceBehaviour.cs
+++ Assets/Scripts/BounceBehaviour.cs
@@ -9,7 +9,7 @@ public class BounceBehaviour : MonoBehaviour
 	{		
 		if(coll.tag == "Player" && Player.GetComponent<Rigidbody2D>().velocity.y != 0)
 		{
-			Player.GetComponent<Rigidbody2D>().AddForce(new Vector2(Player.GetComponent<Rigidbody2D>().velocity.x*2, 900.0f));
+			Player.GetComponent<Rigidbody2D>().AddForce(new Vector2(Player.GetComponent<Rigidbody2D>().velocity.x*2, 13400.0f));
 		}
 	}
 	// Use this for initialization
